cd00f3587d854850df84f8b191ff60a127bcb83e
Christian Motz <christian.motz@gmx.at>
2020-12-17 15:06:50 +0100

    completed tests for in-mem-repo

--- api/core/use_cases/stub/StubInMemRepository.js
+++ api/core/use_cases/stub/StubInMemRepository.js
@@ -5,16 +5,22 @@ const IGateway = require('../../gateways/IGateway');
 class StubInMemRepository extends IGateway {
     constructor() {
         super();
-        this.repo = {};
+        this.repo = [];
     }
-    save(stub_entity) { this.repo[stub_entity.id] = stub_entity; }
-    load(id) { return this.repo[id]; }
+    save(stub_entity) {
+        this.repo.push(stub_entity);
+    }
+    load(id) {
+        const entity_found = this.repo.find(e => e.id == id);
+        return entity_found;
+    }
     remove(id) {
-        const e = load(id);
-        delete this.repo[id];
-        return e;
+        const entitiy_remove = this.load(id);
+        this.repo = this.repo.filter(e => e.id != id);
+        return entitiy_remove;
     }
     size() { return this.repo.length; }
+    clear() { this.repo = []; }
 }
 
 module.exports = StubInMemRepository;
