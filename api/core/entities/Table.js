f5694dd51b5b9ceab69131fa8ae4f205cc312adb
Christian Motz <christian.motz@gmx.at>
2020-12-21 00:39:09 +0100

    entities for the guest registration

---
+++ api/core/entities/Table.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Table {
+    constructor(id, name, location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+}
+
+module.exports = Table;
