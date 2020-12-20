2a7ea77ee4721eb852029f54b3b361efd087142f
chwuggenig <chwuggenig@edu.aau.at>
2020-12-20 11:37:41 +0100

    fixed failing tests for stub

--- api/test/core/use_cases/stub/StubInteractorWrongRequests.test.js
+++ api/test/core/use_cases/stub/StubInteractorWrongRequests.test.js
@@ -2,6 +2,7 @@ const StubValidator = require('../../../../core/use_cases/stub/StubValidator')
 const FakeStubInMemRepository = require('./fake/FakeStubInMemRepository')
 const StubInteractor = require('../../../../core/use_cases/stub/StubInteractor')
 const StubRequestModel = require('../../../../core/use_cases/stub/StubRequestModel')
+const StubEntity = require('../../../../core/entities/StubEntity')
 
 let stub_validator = new StubValidator()
 let stub_repo = new FakeStubInMemRepository();
@@ -24,6 +25,8 @@ test('should create error message in response if id is less than zero', () => {
 })
 
 test('should not create error message in response if id is zero', () => {
+    let entity = new StubEntity(0, "");
+    stub_repo.save(entity);
     let stub_req = new StubRequestModel(0)
     const stub_response = stub_interactor.execute(stub_req)
     expect(stub_response.id).toBe(0)
@@ -31,6 +34,8 @@ test('should not create error message in response if id is zero', () => {
 })
 
 test('should not create error message in response if id is one', () => {
+    let entity = new StubEntity(1, "");
+    stub_repo.save(entity);
     let stub_req = new StubRequestModel(1)
     const stub_response = stub_interactor.execute(stub_req)
     expect(stub_response.id).toBe(1)
