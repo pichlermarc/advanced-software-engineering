3dd341b3bcc95f09d07e6e75cd95bb38b5ee15f2
Christian Motz <christian.motz@gmx.at>
2020-12-22 15:36:13 +0100

    refactored test: exchanged entity stub against location

--- api/test/core/use_cases/AddLocationInteractorTest.test.js
+++ api/test/core/use_cases/AddLocationInteractorTest.test.js
@@ -2,43 +2,36 @@ const AddLocationValidator = require('../../../core/validation/AddLocationValida
 const FakeLocationsInMemRepository = require('../repositories/fake/FakeLocationsInMemRepository')
 const AddLocationInteractor = require('../../../core/use_cases/AddLocationInteractor')
 const AddLocationRequestModel = require('../../../core/requestModels/AddLocationRequestModel')
-const StubEntity = require('../../../core/stub/StubEntity');
+const Location = require('../../../core/entities/Location');
 
-const STUB_ID = 4711
-const STUB_NAME = "stub"
+const LOCATION_ID = 4711
+const LOCATION_NAME = "location-dummy"
 
 let validator = new AddLocationValidator()
 let repo = new FakeLocationsInMemRepository();
 let interactor = new AddLocationInteractor(repo, validator)
-let req = new AddLocationRequestModel(STUB_NAME)
-let entity = new StubEntity(STUB_ID, STUB_NAME);
+let entity = new Location(LOCATION_ID, LOCATION_NAME);
 let res;
 
 beforeEach(() => {
-    // fixture setup
     repo.clear();
 })
-
-afterEach(() => {
-    // fixture teardown
-    repo.clear();
-});
 
 test('should create correct response for valid request', () => {
     res = interactor.execute(entity);
     expect(res).toBeDefined()
-    expect(res.name).toBe(STUB_NAME)
+    expect(res.name).toBe(LOCATION_NAME)
     expect(res.error_msg).toBeNull()
 })
 
 test('should save new entity to DB', () => {
     res = interactor.execute(entity);
-    expect(repo.load(res.id)).toStrictEqual(new StubEntity(res.id, STUB_NAME));
+    expect(repo.load(res.id)).toStrictEqual(new Location(res.id, LOCATION_NAME));
 })
 
 test('validator must throw error because location name is empty', () => {
-    entity = new StubEntity(STUB_ID, "");
+    entity = new Location(LOCATION_ID, "");
     res = interactor.execute(entity);
     expect(res.error_msg).toBe("Name must not be empty!");
-    expect(repo.load(STUB_ID)).toBeUndefined();
+    expect(repo.load(LOCATION_ID)).toBeUndefined();
 })
