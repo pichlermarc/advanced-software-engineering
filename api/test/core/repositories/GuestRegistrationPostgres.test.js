4a1b24a1c059f0b217e50f0fdf7396c7432a2bc6
Christian Motz <christian.motz@gmx.at>
2021-01-03 15:21:23 +0100

    refactored db test

--- api/test/core/repositories/GuestRegistrationPostgres.test.js
+++ api/test/core/repositories/GuestRegistrationPostgres.test.js
@@ -3,78 +3,53 @@ const create_connection_pool = require('../../../core/repositories/ConnectionPoo
 const GuestRegistrationPostgres = require('../../../core/repositories/GuestRegistrationPostgres')
 
 const config = create_config()
-const repo = new GuestRegistrationPostgres(config);
-const pool = repo.pool;
-const query_time = "SELECT NOW();"
-const query_math = "SELECT 2*2 AS \"result\";"
+//const repo = new GuestRegistrationPostgres(config);
+const query_maths = "SELECT 2*2 AS \"result\";"
+const query_not_exists = "select * from this_table_does_not_exist;"
+
+const TRANSACTION_BEGIN = "BEGIN";
+const TRANSACTION_ROLLBACK = "ROLLBACK";
 
 console.log("config : " + JSON.stringify(config));
 
-/*
-test('should execute a simple query', () => {
-    pool.query(query_time, (err, res) => {
-        if (err) {
-            throw err
-        }
-        console.log("It's ", res, " now.")
-    })
-})
-*/
+let pgPool;
 
-/*
-test('should execute a simple query', () => {
-    pool
-        .query(query_time)
-        .then(res => console.log("It's ", res, " now."))
-        .catch(err =>
-            setImmediate(() => {
-                throw err
-            })
-        )
-})
-*/
-
-/*test('should execute a simple query', () => {
-    pool.query(query_time)
-        .then(res => {console.log("It's ", res, " now.")})
-        .catch();
-})*/
-/*
-test('should execute a simple query - promises', () => {
-    pool.query(query_math)
-        .then(res => {console.log("It's ", res, " now.")})
-        .catch(e => {console.error(e)});
-})
-*/
-
-describe('testing postgres', () => {
-
-    let pgPool;
-
-    beforeAll(() => {
-        pgPool = create_connection_pool(config)
-    });
-
-    afterAll(async () => {
-        await pgPool.end();
-    });
-
-    it('should test', async () => {
-        const client = await pgPool.connect();
-        try {
-            await client.query('BEGIN');
-
-            const { rows } = await client.query(query_math);
-            console.log("result of query \"", query_math, "\":", rows[0]["result"])
-            expect(rows[0]["result"]).toBe(4);
-
-            await client.query('ROLLBACK');
-        } catch(err) {
-            throw err;
-        } finally {
-            client.release();
-        }
-
-    })
-
+beforeAll(() => {
+    pgPool = create_connection_pool(config)
 });
+
+afterAll(async () => {
+    await pgPool.end();
+});
+
+
+test('should query simple maths', async () => {
+    const client = await pgPool.connect();
+    try {
+        await client.query(TRANSACTION_BEGIN);
+
+        const { rows } = await client.query(query_maths);
+        console.log("result of query \"", query_maths, "\":", rows[0]["result"])
+        expect(rows[0]["result"]).toBe(4);
+
+        await client.query(TRANSACTION_ROLLBACK);
+    } catch(err) {
+        throw err;
+    } finally {
+        await client.release();
+    }
+})
+
+test('should throw error if query not existing table', async () => {
+    const client = await pgPool.connect();
+    try {
+        await client.query(TRANSACTION_BEGIN);
+        const { rows } = await client.query(query_not_exists);
+    } catch(err) {
+        expect(err).toBeDefined();
+        expect(err.toString()).toBe("error: relation \"this_table_does_not_exist\" does not exist");
+    } finally {
+        await client.query(TRANSACTION_ROLLBACK);
+        await client.release();
+    }
+})
