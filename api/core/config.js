b6475180ef79b579809a41840a1fd83e53db0caf
Christian Motz <christian.motz@gmx.at>
2021-01-03 02:11:12 +0100

    simple config file for different env-types

---
+++ api/core/config.js
@@ -0,0 +1,44 @@
+/**
+ *      Example from:
+ *      https://codeburst.io/node-js-best-practices-smarter-ways-to-manage-config-files-and-variables-893eef56cbef
+ *      https://codingsans.com/blog/node-config-best-practices
+ *
+ *      default-config = development.
+ *      All other configs override the development values.
+ */
+
+const config =
+{
+    // development: local development
+    development: {
+        config_id: "development",
+        app_name: "guest registration",
+        app_desc: "ase project",
+        db_name: "development",
+        db_passwd: "postgres",
+        db_schema: "public",
+        db_port: 5051
+    },
+    testing: {
+        config_id: "testing",
+        database: "my-app-db-test"
+    },
+    staging: {
+        config_id: "staging",
+        database: "my-app-db-stag"
+    },
+    production: {
+        config_id: "production",
+        database: "my-app-db-prod"
+    }
+};
+
+const default_config = config.development;
+
+// Factory 'create_config' returns the merged result of default-config and wanted-config
+const create_config = (config_type=null) => {
+    const wanted_config = config[config_type] || {};
+    return Object.assign({}, default_config, wanted_config)
+}
+
+module.exports = create_config;
---
+++ api/test/config.test.js
@@ -0,0 +1,33 @@
+const create_config = require('../core/config')
+
+
+test('should return default config', () => {
+    const cnf = create_config("development")
+    expect(cnf).toBeDefined()
+    expect(cnf.config_id).toBe("development")
+})
+
+test('should return production config', () => {
+    const cnf = create_config("production")
+    expect(cnf.config_id).toBe("production")
+})
+
+test('should return testing config', () => {
+    const cnf = create_config("testing")
+    expect(cnf.config_id).toBe("testing")
+})
+
+test('should return default config if config_type is empty string', () => {
+    const cnf = create_config("")
+    expect(cnf.config_id).toBe("development")
+})
+
+test('should return default config if config_type not passed', () => {
+    const cnf = create_config()
+    expect(cnf.config_id).toBe("development")
+})
+
+test('should return default config if passed wrong type for config_type', () => {
+    const cnf = create_config(true)
+    expect(cnf.config_id).toBe("development")
+})
