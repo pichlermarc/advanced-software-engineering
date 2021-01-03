0026237220765ea4ddb274df9171a99efb92ab6e
Christian Motz <christian.motz@gmx.at>
2021-01-03 15:48:30 +0100

    refactoring tests not working

--- api/test/core/repositories/GuestRegistrationPostgres.test.js
+++ api/test/core/repositories/GuestRegistrationPostgres.test.js
@@ -6,6 +6,7 @@ const config = create_config()
 //const repo = new GuestRegistrationPostgres(config);
 const query_maths = "SELECT 2*2 AS \"result\";"
 const query_not_exists = "select * from this_table_does_not_exist;"
+const query_all_locations = "select * from locations;"
 
 const TRANSACTION_BEGIN = "BEGIN";
 const TRANSACTION_ROLLBACK = "ROLLBACK";
@@ -44,7 +45,7 @@ test('should throw error if query not existing table', async () => {
     const client = await pgPool.connect();
     try {
         await client.query(TRANSACTION_BEGIN);
-        const { rows } = await client.query(query_not_exists);
+        await client.query(query_not_exists);
     } catch(err) {
         expect(err).toBeDefined();
         expect(err.toString()).toBe("error: relation \"this_table_does_not_exist\" does not exist");
@@ -52,4 +53,40 @@ test('should throw error if query not existing table', async () => {
         await client.query(TRANSACTION_ROLLBACK);
         await client.release();
     }
+})
+
+const query_executor = async (query_to_exec, succ_callback, err_callback) => {
+    const client = await pgPool.connect();
+    try {
+        await client.query(TRANSACTION_BEGIN);
+        const { rows } = await client.query(query_to_exec);
+        expect(rows).toBeDefined();
+        succ_callback()
+    } catch(err) {
+        expect(err).toBeDefined();
+        err_callback()
+    } finally {
+        await client.query(TRANSACTION_ROLLBACK);
+        await client.release();
+    }
+}
+
+test('should query simple maths with executor', async () => {
+    query_executor(query_all_locations,
+        succ = async () => {
+            expect(rows[0]["result"]).toBe(4);
+    })
+})
+
+test('should fail query simple wrong maths with executor', async () => {
+    query_executor(query_all_locations,
+        succ = async () => {
+            expect(rows[0]["result"]).toBe(3);
+        })
+})
+
+test('should throw error if query not existing table with executor', async () => {
+    query_executor(query_not_exists, null, err_fct = async () => {
+        expect(err.toString()).toBe("error: relation \"this_table_does_not_exist\" does not exist");
+    })
 })
