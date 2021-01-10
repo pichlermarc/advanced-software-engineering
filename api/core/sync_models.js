ceb7eb05e42b94d02d5ba911129825ab858da11a
ankopein <ankopein@edu.aau.at>
2021-01-10 16:51:36 +0100

    Fix asyncJS syntax in sync_models + close DB connection

--- api/core/sync_models.js
+++ api/core/sync_models.js
@@ -10,20 +10,21 @@ const {sequelize} = require("./repositories/models")
 
 
 async function sync_models(sync_options = null) {
+    try {
+        if (sync_options == null) {
+            sync_options = {force: true};
+        }
+        console.log("sync-options: ", sync_options);
 
-    if(sync_options == null) {
-        sync_options = {force: true};
-    }
-    console.log("sync-options: ", sync_options);
-
-    console.log("Try sync models with postgres database...")
-    await sequelize.sync(sync_options)
-        .then(() => {
-            console.log("Synced models OK")
-        }).catch(err => {
-            console.error('Sync: Unable to connect to the database:', err);
-            process.exit;
-        });
+        console.log("Try sync models with postgres database...")
+        await sequelize.sync(sync_options)
+        console.log("Synced models OK")
+        sequelize.close();
+    } catch(err) {
+        console.error('Sync: Unable to connect to the database:', err);
+        sequelize.close();
+        process.exit;
+    };
 }
 
 sync_models();
