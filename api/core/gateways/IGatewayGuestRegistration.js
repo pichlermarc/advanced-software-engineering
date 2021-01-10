546da0b221328d9b75d976ccfd4e8fec4569196a
Christian Motz <christian.motz@gmx.at>
2021-01-10 18:26:04 +0100

    added all CRUD to IGateway

--- api/core/gateways/IGatewayGuestRegistration.js
+++ api/core/gateways/IGatewayGuestRegistration.js
@@ -2,14 +2,20 @@
 
 class IGatewayLocation {
 
+    // CRUD ... create, read, update, delete
+
     save_location(location) {}
     load_location(id) {}
+    update_location(id) {}
     remove_location(id) {}
+    load_all_locations() {}
     size_location() {}
 
     save_table(table) {}
     load_table(id) {}
+    update_table(id) {}
     remove_table(id) {}
+    load_all_tables(location_id) {}
     size_table() {}
 
     save_assign_g2t(assign) {}
