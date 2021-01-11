559bd94228d05485d40abe398a48fb981b792c58
Marc Pichler <marcpi@edu.aau.at>
2021-01-11 22:16:26 +0100

    fixed path errors in execute-integration-tests.bash

--- api/script/execute-integration-tests.bash
+++ api/script/execute-integration-tests.bash
@@ -2,13 +2,13 @@
 
 #set -vx
 
+
 echo "----- INTEGRATION TESTS -----"
 echo "Wait for DB container has started"
-sleep 1
+sleep 10
 echo "....."
 
 export NODE_ENV=staging
-cd ./api
 npm run sync
-bash ./node_modules/.bin/jest ./api/test --notify --config=./jest.config.js --runInBand --coverage
+jest ./test --notify --config=./jest.config.js --runInBand --coverage
 
