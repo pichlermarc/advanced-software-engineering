e6405485e9562e0e8e27cd0674c976fc65a2f66d
ankopein <ankopein@edu.aau.at>
2021-01-11 14:12:56 +0100

    Enable using env-variable POSTGRES_HOSTNAME for DB connection

--- api/core/repositories/index.js
+++ api/core/repositories/index.js
@@ -8,14 +8,26 @@ let connection = null;
 const create_db_connection = (config) => {
     if(!connection) {
         try {
-            connection = new Sequelize({
-                database: config.db_name,
-                username: config.db_user,
-                password: config.db_passwd,
-                host: config.host_name,
-                port: config.db_port,
-                dialect: config.db_dialect,
-        });
+            const host_name = process.env.POSTGRES_HOSTNAME;
+            if (host_name) { // postgres hostname from env-variable for CI
+                connection = new Sequelize({
+                    database: config.db_name,
+                    username: config.db_user,
+                    password: config.db_passwd,
+                    host: host_name,
+                    port: config.db_port,
+                    dialect: config.db_dialect,
+                });
+            } else { // postgres hostname default from config
+                connection = new Sequelize({
+                    database: config.db_name,
+                    username: config.db_user,
+                    password: config.db_passwd,
+                    host: config.host_name,
+                    port: config.db_port,
+                    dialect: config.db_dialect,
+                });
+            }
         } catch (ex) {
             console.error(__filename, ex);
             process.exit(1);
