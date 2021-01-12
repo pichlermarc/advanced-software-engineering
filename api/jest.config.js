7d6ba2e466b062b0449f59c8e4a221371294d764
Christian Motz <christian.motz@gmx.at>
2021-01-13 00:39:40 +0100

    played with jest-config

--- api/jest.config.js
+++ api/jest.config.js
@@ -2,7 +2,27 @@
 
 module.exports = {
   verbose: false,
+
+  "collectCoverage": true,
+
+  "coverageThreshold": {
+    // requested global coverage standards
+    "global": {
+        "branches": 95,
+        "functions": 100,
+        "lines": 100,
+    },
+    // requested 
+    "./core/repositories/GuestRegistrationPostgres.js": {
+            "branches": 95,
+            "functions": 100,
+            "lines": 100,
+    },
+  },
+
+  // output directory for coverage report
+  "coverageDirectory": "",
   
   "collectCoverageFrom": [
-  ]
+  ],
 };
