d4fb56d1c4af6ed6e20ce9492385507e6c68be9f
Christian Motz <christian.motz@gmx.at>
2021-01-14 20:25:37 +0100

    added 'exit 0': good bash-script-termination

--- api/script/execute-integration-tests.bash
+++ api/script/execute-integration-tests.bash
@@ -3,12 +3,24 @@
 
 echo "----- INTEGRATION TESTS -----"
 echo "Wait for DB container has started"
-sleep 12
+#sleep 12
 echo "=> Script continues executing tests..."
 
+
 export NODE_ENV=staging
+
+# ----- only for local use - START -----
 #export NODE_ENV=test
+# ----- only for local use - END -----
+
 # NOTE: sync needs the env-var NODE_ENV
 npm run sync
-jest ./test --config=./jest.config.js --runInBand --coverage
+
+echo "Run unit-tests first:"
+jest ./test --config=./jest.config.js --runInBand --coverage --group=-integrationtest
+
+echo "Run integration-tests next:"
+jest ./test --config=./jest.config.js --runInBand --coverage --group=integrationtest
+
+exit 0
 
