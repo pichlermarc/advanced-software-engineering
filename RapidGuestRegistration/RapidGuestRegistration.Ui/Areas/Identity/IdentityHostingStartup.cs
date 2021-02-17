53ca7f59690f8d602612776c6ba087dbefa69e01
andimairitsch <anmairitsch@edu.aau.at>
2021-02-17 22:14:31 +0100

    vergessene Files hinzugefügt

---
+++ RapidGuestRegistration/RapidGuestRegistration.Ui/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RapidGuestRegistration.Ui.Data;
+
+[assembly: HostingStartup(typeof(RapidGuestRegistration.Ui.Areas.Identity.IdentityHostingStartup))]
+namespace RapidGuestRegistration.Ui.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+
+        public IdentityHostingStartup()
+        {
+        }
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(async (context, services) =>  {
+
+               
+            });
+
+        }
+    }
+}
+
